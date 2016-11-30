7915eded3a6eeca7c798989bb4d5f446fd62644e
mkwng <hello@mkwng.com>
2016-11-29 21:41:49 -0500

    favicon

--- src/App.js
+++ src/App.js
@@ -8,12 +8,13 @@ class App extends Component {
   render() {
     return (
       <div className="vh-100 pa4 mw7 center">
-        <h1 className="f5 mv4 sans-serif">Are you a Tachyons Pro?</h1>
+        <h1 className="f5 mt3 mb4 sans-serif">Are you a Tachyons Pro?</h1>
         <p className="f5 mt4 mb5 sans-serif">Learn Tachyons by memorizing the class names. <a href="http://tachyons.io" target="_blank" className="blue">What is Tachyons?</a></p>
 
         <TerminalWindow questions={ QUESTIONS } myData={ myData } />
 
         <p className="f6 mt5 sans-serif">See an error? <a href="https://github.com/mkwng/tachyons-quiz" target="_blank" className="blue">Submit an issue</a></p>
+        <p className="f6 sans-serif">Say hi on Twitter: <a href="https://twitter.com/mkwng/" target="_blank" className="blue" >@mkwng</a></p>
 
       </div>
     );
